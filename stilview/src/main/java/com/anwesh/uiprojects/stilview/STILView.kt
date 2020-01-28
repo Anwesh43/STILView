2234372efce555bf946542b3d2f46fc29b1f22c3
Anwesh43 <anweshthecool0@gmail.com>
2020-01-28 10:55:05 +0530

    adding constants and libraries

--- stilview/src/main/java/com/anwesh/uiprojects/stilview/STILView.kt
+++ stilview/src/main/java/com/anwesh/uiprojects/stilview/STILView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.stilview
  * Created by anweshmishra on 28/01/20.
  */
 
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val lines : Int = 4
+val scGap : Float = 0.02f / lines
+val sizeFactor : Float = 3f
+val strokeFactor : Int = 90
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val triSizeFactor : Float = 5f
+
