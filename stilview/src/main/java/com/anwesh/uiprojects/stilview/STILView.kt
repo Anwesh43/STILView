c933d92327bccc0f826d642d5b02df62f6399fcc
Anwesh43 <anweshthecool0@gmail.com>
2020-01-28 11:04:49 +0530

    adding Canvas extension methods

--- stilview/src/main/java/com/anwesh/uiprojects/stilview/STILView.kt
+++ stilview/src/main/java/com/anwesh/uiprojects/stilview/STILView.kt
@@ -25,3 +25,52 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawArrow(size : Float, paint : Paint) {
+    for (j in 0..1) {
+        save()
+        rotate(45f * (1 - 2 * j))
+        drawLine(0f, 0f, size, 0f, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawSquareLineArrow(i : Int, size : Float, scale : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val scDiv : Double = 1.0 / lines
+    val j : Int = Math.floor(sf / scDiv).toInt()
+    val sfi : Float = sf.divideScale(j, lines)
+    val x : Float = 2 * size * sfi
+    save()
+    rotate(90f * i)
+    save()
+    translate(-size, -size)
+    drawLine(0f, 0f, x, 0f, paint)
+    if (i == j) {
+        save()
+        translate(x, 0f)
+        drawArrow(size / triSizeFactor, paint)
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawSquareLines(size : Float, scale : Float, paint : Paint) {
+    for (j in 0..(lines - 1)) {
+        drawSquareLineArrow(j, size, scale, paint)
+    }
+}
+
+fun Canvas.drawSTIL(scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, h / 2)
+    drawSquareLines(size, scale, paint)
+    restore()
+}
