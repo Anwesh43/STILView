3487b85bdc076412774747d2148d8b7c217ada73
Anwesh43 <anweshthecool0@gmail.com>
2020-01-28 11:17:29 +0530

    adding STILView in Activity

--- app/src/main/java/com/anwesh/uiprojects/squaretriindicatorlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/squaretriindicatorlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.squaretriindicatorlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.stilview.STILView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        STILView.create(this)
     }
 }
